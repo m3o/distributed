b43abd0486e21c34064b3fd77fae021770a89b67
Zoran Plesivcak <zoran@shard.systems>
2021-04-19 00:03:14 +0100

    fix: Make sure /.next and /public are ignored by ESlint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,7 +30,7 @@ module.exports = {
       version: 'detect',
     },
   },
-  ignorePatterns: ['/public/**'],
+  ignorePatterns: ['/public/**', '/.next/**'],
   rules: {
     semi: ['error', 'never'],
     'import/order': [
