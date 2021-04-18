357f6765fb3f146d7877d43405bd3dd92fcd2423
Zoran Plesivcak <zoran@shard.systems>
2021-04-18 21:05:10 +0100

    feat: Configure ESlint and run it in pre-commit hook

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['/public/**'],
+  rules: {
+    semi: ['error', 'never'],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-undef': 'off',
+      },
+    },
+  ],
+}
